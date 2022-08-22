56c50cc614f3348ea23ca954c1a168d0572cb79b
frank1010111 <frmale@utexas.edu>
2022-08-22 17:22:28 -0400

    move storage to .viceroy

--- random_tweet.sh
+++ random_tweet.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #source /home/frank/anaconda3/bin/activate tweepy
 
-QUEUE_FILE=tweet_queue.json
+QUEUE_FILE=.viceroy/tweet_queue.json
 python random_tweet.py $QUEUE_FILE && \
     echo "RANDOM TWEET SENT"
