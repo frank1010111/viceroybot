5088186647e9850453db6df2641cd5914fbc88e1
frank1010111 <frmale@utexas.edu>
2022-08-26 00:15:05 -0400

    :construction: rust markov functions that definitely do not work

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+use std::collections::HashMap;
+use rand::seq::SliceRandom;
+
+
+use pyo3::{
+    pymodule,
+    types::{PyModule, PyString, PyDict},
+    PyResult, Python,
+};
+
+
+#[pymodule]
+fn markov(_py: Python, m: &PyModule) -> PyResult<()> {
+
+
+    fn predict(
+        markov_chain: HashMap<Vec<String>, Vec<String>>, n_words: i32, prompt: String
+    ) -> String {
+        let short_prefixes = short_prefix_chain(&markov_chain);
+        let n_prefix = markov_chain.keys().nth(0).expect("prefixes are not formed").len();
+        let mut output: Vec<String> = prompt.split_whitespace().map(|s| s.to_string()).collect();
+
+
+        for word in 0..n_words {
+            if output.len() >= n_prefix {
+                let prefix = output.iter().rev().take(n_prefix);
+                output.push(markov_chain[prefix].choose(&mut rand::thread_rng()).to_string())
+            }
+            else {
+                let prefix = output.iter().last();
+
+            }
+
+
+        }
+        "X".to_string()
+    }
+
+    fn update_chain(line: String, mut chain: HashMap<Vec<String>, Vec<String>>, n_prefix: usize) -> HashMap<Vec<String>, Vec<String>> {
+        let words: Vec<String> = line.split_whitespace().map(|s| s.to_string()).collect();
+        if words.len() < (n_prefix + 1) {
+            return chain
+        }
+        for i in n_prefix..words.len() {
+            let prefix = Vec::from(&words[(i - n_prefix)..i]);
+            let prediction = &words[i];
+            chain.entry(prefix).and_modify(|pred| pred.push(prediction.to_string())).or_insert(vec![prediction.to_string()]);
+        }
+        chain
+    }
+
+
+    // wrapper
+    #[pyfn(m)]
+    #[pyo3(name = "predict")]
+    fn predict_py<'py>(
+        _py: Python<'py>,
+        markov_chain: &PyDict,
+        n_words: i32,
+        prompt: &PyString,
+    ) -> PyResult<String>{
+        let markov_chain = markov_chain.extract()?;
+        let prompt = prompt.extract()?;
+
+        let output = predict(markov_chain, n_words, prompt);
+        Ok(output)
+    }
+
+    Ok(())
+}
+
+/// Generate a chain from the last element in the prefix to the list of possibilities.
+fn short_prefix_chain(chain: &HashMap<Vec<String>, Vec<String>>) -> HashMap<String, Vec<String>> {
+    let mut short_prefix: HashMap<String,Vec<String>> = HashMap::new();
+    for (prefix, postfix) in chain.iter() {
+        let last = prefix.iter().last().expect("Missing prefix").to_string();
+        short_prefix.entry(last)
+            .and_modify(|pred| pred.extend(postfix.iter().cloned()))
+            .or_insert(postfix.to_vec());
+    }
+    short_prefix
+}
